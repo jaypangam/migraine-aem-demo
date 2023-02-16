fa9853781b0a38b0f373162ea11f9d61ff757b77
jaypangam <jay.pangam@pfizer.com>
2023-02-16 15:48:57 +0530

    video block

--- blocks/top-video-text-overlay-and-button/top-video-text-overlay-and-button.js
+++ blocks/top-video-text-overlay-and-button/top-video-text-overlay-and-button.js
@@ -6,41 +6,13 @@ export default function decorate(block) {
   const script = document.createElement("script");
 
   script.src =
-    "https://players.brightcove.net/1852113022001/BrBLrQfcq_default/index.min.js";
+    "https://players.brightcove.net/1852113022001/g2OtgoAoBs_default/index.min.js";
 
   container.append(script);
   container.style.padding = "0px";
   let desktopVideo = videoInit(container.dataset.videoId, 'desktop-video')
   container.appendChild(desktopVideo);
 
-  const control = document.createElement("div");
-  control.className = "pause";
-  container.prepend(control);
-
-  let firstButton = container.querySelector(".button-container:first-child a");
-  firstButton.classList.remove("button");
-  firstButton.classList.add("text-white");
-
-  let cta = container.querySelector(".top-video-text-overlay-and-button a");
-  cta.classList.add("cta");
-  cta.closest('div').classList.add("cta-wrapper");
-  const player = document.querySelector("video-js");
-
-  player.addEventListener("click", (e) => {
-    const isPaused = e.target.parentElement.classList.contains("vjs-paused");
-    let control = document.querySelector(
-      ".top-video-text-overlay-and-button-container > div"
-    );
-
-    if (!isPaused) {
-      control.classList.remove("pause");
-      control.classList.add("play");
-      return;
-    }
-
-    control.classList.remove("play");
-    control.classList.add("pause");
-  });
 }
 
 function videoInit(videoId, classes) {
@@ -51,12 +23,12 @@ function videoInit(videoId, classes) {
   const video = document.createElement("video-js");
 
   video.setAttribute("data-account", "1852113022001");
-  video.setAttribute("data-player", "BrBLrQfcq");
+  video.setAttribute("data-player", "g2OtgoAoBs");
   video.setAttribute("data-embed", "default");
   video.setAttribute("controls", "");
   video.setAttribute("autoplay", false);
-  video.setAttribute("loop", true);
-  video.setAttribute("muted", true);
+  video.setAttribute("loop", false);
+  video.setAttribute("muted", false);
   video.setAttribute("preload", true);
   video.setAttribute("data-video-id", videoId);
   video.setAttribute("data-playlist-id", "");
